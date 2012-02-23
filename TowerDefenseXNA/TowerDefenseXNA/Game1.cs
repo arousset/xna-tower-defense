6fd1f529a59cdda79d2c4e233d3a4db7cde9dc8a
clement.kawczak@gmail.com <clement.kawczak@gmail.com@45743e6b-2e15-57ff-5a00-f668c8d01507>
2012-02-23 17:42:15 +0000

    Mise en place de la configuration du projet Ajout des réferences vers La librairie TiledLib GS4 pour la lecture des fichiers .mtx Ajout d'une map d'exemple

---
+++ TowerDefenseXNA/TowerDefenseXNA/Game1.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using TiledLib;
+
+namespace TowerDefenseXNA
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Map map;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            this.graphics.IsFullScreen = false;
+            this.graphics.PreferredBackBufferWidth = 960;
+            this.graphics.PreferredBackBufferHeight = 480;
+            this.graphics.ApplyChanges();
+            this.Window.Title = "Tower Defense";
+            this.Window.AllowUserResizing = false;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            map = Content.Load<Map>("Map");
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            map.Draw(spriteBatch);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
