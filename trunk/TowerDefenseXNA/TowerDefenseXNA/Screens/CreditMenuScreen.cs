3ad41766a2dd21338ce209f2f5b2e3876cfb4897
clement.kawczak@gmail.com <clement.kawczak@gmail.com@45743e6b-2e15-57ff-5a00-f668c8d01507>
2012-03-16 16:22:20 +0000

    Modification de l'achitecture du projet. Ajout de menu et menu manager.

---
+++ trunk/TowerDefenseXNA/TowerDefenseXNA/Screens/CreditMenuScreen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameStateManagement
+{
+    class CreditMenuScreen : MenuScreen
+    {
+        public CreditMenuScreen()
+            : base("Credit")
+        {
+            MenuEntry back = new MenuEntry("Back");
+            back.Selected += OnCancel;
+            MenuEntry alban = new MenuEntry("Alban Rousset");
+            MenuEntry clement = new MenuEntry("Clement Kawczak");
+
+            MenuEntries.Add(alban);
+            MenuEntries.Add(clement);
+            MenuEntries.Add(back);
+        }
+    }
+}
---
+++ trunk/TowerDefenseXNA/TowerDefenseXNA/Screens/MainMenuScreen.cs
@@ -0,0 +1,116 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// MainMenuScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using Microsoft.Xna.Framework;
+#endregion
+
+namespace GameStateManagement
+{
+    /// <summary>
+    /// The main menu screen is the first thing displayed when the game starts up.
+    /// </summary>
+    class MainMenuScreen : MenuScreen
+    {
+        #region Initialization
+
+
+        /// <summary>
+        /// Constructor fills in the menu contents.
+        /// </summary>
+        public MainMenuScreen()
+            : base("Tower Defense")
+        {
+            // Create our menu entries.
+            MenuEntry playGameMenuEntry = new MenuEntry("Play Game solo");
+            MenuEntry multiMenuEntry = new MenuEntry("Multiplayer");
+            MenuEntry optionsMenuEntry = new MenuEntry("Options");
+            MenuEntry creditMenuEntry = new MenuEntry("Credit");
+            MenuEntry exitMenuEntry = new MenuEntry("Exit");
+
+            // Hook up menu event handlers.
+            playGameMenuEntry.Selected += PlayGameMenuEntrySelected;
+            multiMenuEntry.Selected += MultiMenuEntrySelected;
+            optionsMenuEntry.Selected += OptionsMenuEntrySelected;
+            creditMenuEntry.Selected += CreditMenuEntrySelected;
+            exitMenuEntry.Selected += OnCancel;
+
+            // Add entries to the menu.
+            MenuEntries.Add(playGameMenuEntry);
+            MenuEntries.Add(multiMenuEntry);
+            MenuEntries.Add(optionsMenuEntry);
+            MenuEntries.Add(creditMenuEntry);
+            MenuEntries.Add(exitMenuEntry);
+        }
+
+
+        #endregion
+
+        #region Handle Input
+
+
+        /// <summary>
+        /// Event handler for when the Play Game menu entry is selected.
+        /// </summary>
+        void PlayGameMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            LoadingScreen.Load(ScreenManager, true, e.PlayerIndex,
+                               new GameplayScreen());
+        }
+
+        void MultiMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.AddScreen(new OptionsMenuScreen(), e.PlayerIndex);
+        }
+
+        void CreditMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.AddScreen(new CreditMenuScreen(), e.PlayerIndex);
+        }
+
+
+
+        /// <summary>
+        /// Event handler for when the Options menu entry is selected.
+        /// </summary>
+        void OptionsMenuEntrySelected(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.AddScreen(new OptionsMenuScreen(), e.PlayerIndex);
+        }
+
+
+
+        /// <summary>
+        /// When the user cancels the main menu, ask if they want to exit the sample.
+        /// </summary>
+        protected override void OnCancel(PlayerIndex playerIndex)
+        {
+            const string message = "Are you sure you want to exit this sample?";
+
+            MessageBoxScreen confirmExitMessageBox = new MessageBoxScreen(message);
+
+            confirmExitMessageBox.Accepted += ConfirmExitMessageBoxAccepted;
+
+            ScreenManager.AddScreen(confirmExitMessageBox, playerIndex);
+        }
+
+
+        /// <summary>
+        /// Event handler for when the user selects ok on the "are you sure
+        /// you want to exit" message box.
+        /// </summary>
+        void ConfirmExitMessageBoxAccepted(object sender, PlayerIndexEventArgs e)
+        {
+            ScreenManager.Game.Exit();
+        }
+
+
+        #endregion
+    }
+}
